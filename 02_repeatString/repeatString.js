90f39ccf9ee99f761bc6d5431bcf92a7248f7507
Jonathan Moody <jonathanmoody15@gmail.com>
2024-05-20 01:28:30 -0400

    created repeatString function in repeatString.js
    added function calls in repeatString.js
    
    passed all tests in repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,29 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  let result = "";
 
+  if (num < 0) {
+    result += "ERROR";
+  } else {
+    for (i = 0; i < num; i++) {
+      result += str;
+    }
+  }
+
+  return result;
 };
+repeatString("hey", 3); // returns 'heyheyhey'
+
+repeatString("hello", 10);
+
+repeatString("hi", 1);
+
+repeatString("bye", 0);
+
+repeatString("goodbye", -1);
+
+repeatString();
+
+repeatString("", 10);
 
 // Do not edit below this line
 module.exports = repeatString;
