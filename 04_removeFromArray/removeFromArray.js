3ea8868bbbedaeaae4c8bffc43d792ab4abddd40
Jonathan Moody <jonathanmoody15@gmail.com>
2024-05-28 16:15:41 -0400

    sumAll.js: created a function to add the sum of every number between two integers
    sumAll.spec.js: removed .skip from test lines

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...arg) {
+  const newArray = [];
+  array.forEach((element) => {
+    if (!arg.includes(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
 };
 
+// removeFromArray([1, 2, 3, 4], 3, 2, 4);
+// console.log(array);
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let sum = 0;
+  if (Number.isInteger(start && end)) {
+    if (start < 0 || end < 0) {
+      return "ERROR";
+    } else if (start < end) {
+      for (let i = start; i <= end; i++) {
+        sum += i;
+      }
+    }
+    for (let i = end; i <= start; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
