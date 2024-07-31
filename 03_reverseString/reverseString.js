a204b4ee2fb3093a7689366db427fed04a754e44
Jonathan Moody <jonathanmoody15@gmail.com>
2024-07-31 14:27:54 -0400

    removed practice functions and added the splitString method to the reverseString variable

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,21 +1,7 @@
 const reverseString = function (str) {
-  const splitString = str.split("");
-  const reverseString = splitString.reverse().join("");
+  const reverseString = str.split("").reverse().join("");
   return reverseString;
 };
-
-function removePunctuation(str) {
-  return str
-    .split("")
-    .filter((char) => {
-      return /[a-zA-Z]/.test(char);
-    })
-    .join("");
-}
-
-let text = "GeeksforGeeks a computer science portal.";
-let cleanedText = removePunctuation(text);
-console.log(cleanedText);
 
 // Do not edit below this line
 module.exports = reverseString;
