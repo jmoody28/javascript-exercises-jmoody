6a69f32a9482f1cd1c0de886c529a9e3c8fc90cb
Jonathan Moody <jonathanmoody15@gmail.com>
2024-08-01 17:40:43 -0400

    11_getTheTitles: created function to return the the title property of an object in a given array
    12_findTheOldest: created function to return the name of the oldest person in the given array
    
    spec.js: removed skip tag on test lines

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,39 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  const currentYear = new Date().getFullYear();
+  const peeps = array.map((person) => {
+    const age = person.yearOfDeath
+      ? person.yearOfDeath - person.yearOfBirth
+      : currentYear - person.yearOfBirth;
+    return { ...person, age };
+  });
+  const people = peeps.reduce((person, currentPerson) => {
+    if (person.age > currentPerson.age) {
+      return person;
+    } else {
+      return currentPerson;
+    }
+  }, peeps[0]);
+  return people;
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+console.log(findTheOldest(people).name);
 
 // Do not edit below this line
 module.exports = findTheOldest;
